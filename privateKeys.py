dd666a7919b2507a2272a05a73cb7cb1ef2b8af1
Tanvibbsr <Tanvibbsr@gmail.com>
2026-03-29 22:02:32 -0500

    fix errors in privateKeys.py

--- privateKeys.py
+++ privateKeys.py
@@ -1,6 +1,9 @@
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 import time
+import sqlite3
+
+DB_FILE = "totally_not.db"
 
 # Generate RSA key
 def generate_rsa_key():
@@ -20,6 +23,12 @@ def save_key_to_db(pem_key, exp_timestamp):
     conn.commit()
     conn.close()
 
+# Generate key with expiration timestamp
+def generate_key(exp_offset):
+    pem_key, _ = generate_rsa_key()
+    exp_timestamp = int(time.time()) + exp_offset
+    return pem_key, exp_timestamp
+
 # Example: create expired and valid key
-store_key(*generate_key(-10))   # expired
-store_key(*generate_key(3600)) # valid
+save_key_to_db(*generate_key(-10))   # expired
+save_key_to_db(*generate_key(3600)) # valid
